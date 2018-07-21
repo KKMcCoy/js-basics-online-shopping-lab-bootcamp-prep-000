ee912a380f4492d0c4b488ae5f596ee2a1849c18
karen S <buzz.kyt@gmail.com>
2018-07-21 01:51:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,12 +57,7 @@ function removeFromCart(item) {
     cart.splice(i,1);
     return cart;
   }
- 
-// //if (cart.indexOf(item) >= 0) { 
-//     cart.splice(cart.indexOf(item),1);
-//     return cart; 
- 
-  } 
+ }
     return "That item is not in your cart.";
 }
 
