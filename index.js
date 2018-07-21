bc44bc05485428cabd2bca50f44d48ffc7a35dad
karen S <buzz.kyt@gmail.com>
2018-07-21 01:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,10 @@ function total() {
 
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
-  if (cart.indexOf(item) >= 0) { 
+ 
+ 
+ 
+  //if (cart.indexOf(item) >= 0) { 
     cart.splice(cart.indexOf(item),1);
     return cart;
   } else { 
